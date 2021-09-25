0338cc22878697cc6193852e3dc183a3f45e290b
micapierotti <micapierotti@live.com.ar>
2021-09-25 09:28:59 -0300

    first commit

---
+++ src/layouts/dashboard/SidebarConfig.js
@@ -0,0 +1,46 @@
+import { Icon } from '@iconify/react';
+import pieChart2Fill from '@iconify/icons-eva/pie-chart-2-fill';
+import peopleFill from '@iconify/icons-eva/people-fill';
+import peopleOutline from '@iconify/icons-eva/people-outline';
+import shoppingBagFill from '@iconify/icons-eva/shopping-bag-fill';
+import fileTextFill from '@iconify/icons-eva/file-text-fill';
+import settings2Fill from '@iconify/icons-eva/settings-2-fill';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
+
+const sidebarConfig = [
+  {
+    title: 'Home',
+    path: '/dashboard/app',
+    icon: getIcon(pieChart2Fill)
+  },
+  {
+    title: 'Clientes',
+    path: '/dashboard/user',
+    icon: getIcon(peopleOutline)
+  },
+  {
+    title: 'Empleados',
+    path: '/dashboard/employee',
+    icon: getIcon(peopleFill)
+  },
+  {
+    title: 'Productos',
+    path: '/dashboard/products',
+    icon: getIcon(shoppingBagFill)
+  },
+  {
+    title: 'Pedidos',
+    path: '/dashboard/blog',
+    icon: getIcon(fileTextFill)
+  },
+  {
+    title: 'Obras',
+    path: '/dashboard/blog',
+    icon: getIcon(settings2Fill)
+  }
+];
+
+export default sidebarConfig;
