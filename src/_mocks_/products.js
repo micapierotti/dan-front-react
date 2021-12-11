9a0fe75b2fa503366d75134fe51cb9bee5eeb976
Gali Perez <gp.galiperez@gmail.com>
2021-12-11 11:35:37 -0300

    cambios de datos de productos

--- src/_mocks_/products.js
+++ src/_mocks_/products.js
@@ -52,16 +52,9 @@ const products = [...Array(24)].map((_, index) => {
     cover: mockImgProduct(setIndex),
     name: PRODUCT_NAME[index],
     price: faker.datatype.number({ min: 4, max: 99, precision: 0.01 }),
-    priceSale: setIndex % 3 ? null : faker.datatype.number({ min: 19, max: 29, precision: 0.01 }),
-    colors:
-      (setIndex === 1 && PRODUCT_COLOR.slice(0, 2)) ||
-      (setIndex === 2 && PRODUCT_COLOR.slice(1, 3)) ||
-      (setIndex === 3 && PRODUCT_COLOR.slice(2, 4)) ||
-      (setIndex === 4 && PRODUCT_COLOR.slice(3, 6)) ||
-      (setIndex === 23 && PRODUCT_COLOR.slice(4, 6)) ||
-      (setIndex === 24 && PRODUCT_COLOR.slice(5, 6)) ||
-      PRODUCT_COLOR,
-    status: sample(['sale', 'new', '', ''])
+    currentStock: faker.datatype.number({ min: 4, max: 99 }),
+    minStock: faker.datatype.number({ min: 4, max: 99 }),
+    description: 'descripcion aaa 123'
   };
 });
 
